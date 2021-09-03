471c17115e2c7d34d3dc0546aedb20c18a6465ea
Ivan Jovchevski <ivan.jovcevski@yahoo.com>
2021-09-03 18:13:55 +0200

    template i static files

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+posts = [
+    {
+        'author': 'Ivan',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Trajanka',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2019'
+    },
+]
 
+# Create your views here.
+def home(request):
+    context = {
+        "posts": posts,
+        "title": "Blog Home"
+    }
+
+    return render(request, "blog/home.html", context=context)
+
+# Create your views here.
 def about(request):
-    return HttpResponse("<h1> About Blog </h1>")
+    context = {
+        "title": "Blog About"
+    }
+
+    return render(request, "blog/about.html", context=context)
